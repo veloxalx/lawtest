c1cc7d257a8085df8e38b03ce1c37484b20fdcf9
Hashara13 <hnethmin13@gmail.com>
2024-09-16 15:52:32 +0530

    minor changes

--- app/api/lawyer/new/route.js
+++ app/api/lawyer/new/route.js
@@ -1,20 +1,10 @@
-import Lawyer from '@models/lawyer'
-import {connectionDB} from '@utils/db'
-import { stringify } from 'postcss';
-export const POST=async(request)=>{
-    const{userId,
-        lawyerName,
-        age,
-        nic,
-        university,
-        experienceYears,
-        certificate,
-        prevExperiences,
-        experience,
-        profilePic,
-        contactNo}=await request.json();
-    try{
-const newLawyer=new Lawyer({  creator:lawyerName,
+import Lawyer from "@models/lawyer";
+import { connectionDB } from "@utils/db";
+import { stringify } from "postcss";
+export const POST = async (request) => {
+  const {
+    userId,
+    lawyerName,
     age,
     nic,
     university,
@@ -23,12 +13,29 @@ const newLawyer=new Lawyer({  creator:lawyerName,
     prevExperiences,
     experience,
     profilePic,
-    contactNo});
+    contactNo,
+  } = await request.json();
+
+  try {
+    await connectionDB();
+    const newLawyer = new Lawyer({
+      creator: lawyerName,
+      age,
+      nic,
+      university,
+      experienceYears,
+      certificate,
+      prevExperiences,
+      experience,
+      profilePic,
+      contactNo,
+    });
 
     await newLawyer.save();
-    return new Response(JSON.newLawyer(stringify),{status:201})
-    }catch (error) {
-        return new Response("Failed to create a new lawyer profile", { status: 500 });
-}
-
-}
+    return new Response(JSON.newLawyer(stringify), { status: 201 });
+  } catch (error) {
+    return new Response("Failed to create a new lawyer profile", {
+      status: 500,
+    });
+  }
+};
